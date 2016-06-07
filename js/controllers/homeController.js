ca5924e6c149d26875757692cf6754c52cebfdc4
AppleLAN <alanbuscaglia@gmail.com>
2016-06-07 17:07:38 -0300

    New Changes !!!
    fixed some things at the app for the bootcamp

--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -1,10 +1,4 @@
 var app = angular.module('customPage');
 app.controller('homeController',function(textService){
   var vm = this;
-  
-  jQuery('.post').addClass("hidden-div").viewportChecker({
-    classToAdd: 'visible-div animated fadeIn'
-  });
-
-
 });
--- js/directives/textDirective.js
+++ js/directives/textDirective.js
@@ -5,6 +5,11 @@ app.directive('textDirective', function(textService){
     restrict: "E",
     templateUrl: 'js/templates/textDirective.html',
     controller : function ($scope,$timeout){
+      setTimeout(function () {
+        jQuery('.post').addClass("hidden-div").viewportChecker({
+          classToAdd: 'visible-div animated fadeIn'
+        });
+      }, 10);
       $scope.textList = {};
       var promise = textService.getText();
       promise.then(function(data) {
