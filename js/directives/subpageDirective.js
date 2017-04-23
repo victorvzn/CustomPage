e6358213193f1ed4866dab2b434eed7bd94d0ca8
Victor Villazon <victorzn23@gmail.com>
2017-04-23 08:21:51 -0500

    Quitando console.log

--- js/directives/subpageDirective.js
+++ js/directives/subpageDirective.js
@@ -7,15 +7,12 @@ app.directive('subpageDirective', function (subpageService){
       "template": "@"
     },
     templateUrl: function (el, attr) {
-      console.log(attr.template)
       return 'js/templates/' + attr.template + 'Directive.html';
     },
     controller: function ($scope, $timeout) {
       $scope.dataList = {};
-      console.log("controller", $scope.template);
         var promise = subpageService.getText($scope.template);
         promise.then(function (data) {
-            console.log(data.data);
             $scope.dataList = data.data;
         });
     }
